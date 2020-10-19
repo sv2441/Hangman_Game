668668b43016ef5db0c36ff0cad6b6dbc95ee91a
sv2441 <62471058+sv2441@users.noreply.github.com>
2020-10-19 21:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import random
 from words import word_list
-
-
 def get_word():
     word = random.choice(word_list)
     return word.upper()
-
-
 def play(word):
     word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
     tries = 6
-    print("Let's play Hangman!")
+    print("let's play !")
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
